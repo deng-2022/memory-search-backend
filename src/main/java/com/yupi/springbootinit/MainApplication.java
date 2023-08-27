eaf7482cdc8d4ddb2a9740f20900ca255e60a0ba
memory <memory@qq.com>
2023-08-27 13:52:34 +0800

    后端项目初始化

--- src/main/java/com/yupi/springbootinit/MainApplication.java
+++ src/main/java/com/yupi/springbootinit/MainApplication.java
@@ -19,9 +19,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+
 
 }
